46572a56214e1226bddedb03bc55548b551356af
Dan Bright <dan@Dans-MacBook-Pro.local>
2025-03-14 20:53:10 +0000

    Working prototype, with one table.

--- stock_manager/models.py
+++ stock_manager/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    sku = models.CharField(primary_key=True, unique=True, editable=True, max_length=100)
+    description = models.CharField(max_length=250)
+    retail_price = models.FloatField()
+    quantity = models.IntegerField()
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.sku
--- stock_manager/views.py
+++ stock_manager/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Item
+from .serializers import ItemSerializer
+from django.contrib.auth.models import User  # For accessing the User model
+from rest_framework.response import Response  # For returning HTTP responses in REST framework
+from rest_framework.decorators import api_view
 
-# Create your views here.
+# API View
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    lookup_field = 'sku'
+    permission_classes = [IsAuthenticated]
+
+# Main Page View
+@login_required
+def index(request):
+    return render(request, 'index.html')
+
+@api_view(['GET'])
+def get_user(request):
+    # Ensure the user is authenticated
+    if not request.user.is_authenticated:
+        return Response({'detail': 'Authentication credentials were not provided.'}, status=401)
+
+    user = request.user
+    return Response({
+        'id': user.id,
+        'username': user.username,
+        'email': user.email,
+    })
